994836be98174cc5a9cc1c638c07f93448024e0b
l-herrmann <lars.herrmann82@gmail.com>
2020-10-13 13:39:32 +0200

    Update modules.go

--- modules.go
+++ modules.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "github.com/dsouzajude/logspout-fluentd/fluentd"
+	_ "github.com/l-herrmann/logspout-fluentd/fluentd"
 	_ "github.com/gliderlabs/logspout/httpstream"
 	_ "github.com/gliderlabs/logspout/transports/tcp"
 	_ "github.com/gliderlabs/logspout/transports/udp"
